b034a95dea9d097f777f115b85de86aca0b0ddd4
Henrychang26 <henrychang26@gmail.com>
2022-11-19 20:24:51 -0800

    Faucet Back End complete

--- scripts/DeployFaucet.js
+++ scripts/DeployFaucet.js
@@ -3,12 +3,12 @@ const hre = require("hardhat");
 async function main() {
   const Faucet = await hre.ethers.getContractFactory("Faucet");
   const faucet = await Faucet.deploy(
-    "0xe55bEc688192433104974A644CA465FB98391348"
+    "0x0DE9c24141001cB0cA124dbce8A88544d4871536"
   );
 
   await faucet.deployed();
 
-  console.log("Faucet deployed: ", faucet.address);
+  console.log("Faucet Contract deployed: ", faucet.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
